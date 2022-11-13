e0170cf6ad1bfa5702c70c992c0fdb1dc118bb06
lukasz.klejszta <lukasz.klejszta@tsh.io>
2022-11-13 22:58:33 +0100

    feat: apply fix for having multiple it`s in one file

--- 5.1-real-life-scenario/tests/name-age.spec.ts
+++ 5.1-real-life-scenario/tests/name-age.spec.ts
@@ -1,13 +1,18 @@
 /* eslint-disable no-console */
 import { testFramework, StepName } from "./framework/framework";
 import { arrangeAgifyEndpoint, arrangeClearAgeNamePairTable } from "./framework/arrange";
-import { actNameToAgeBatchEndpoint } from "./framework/act";
+import {
+  actAverageAgeEndpoint,
+  actAverageAgeForNameEndpoint,
+  actNameToAgeBatchEndpoint,
+  actRequestsForAgeEndpoint,
+} from "./framework/act";
 import { ApiEndpointName, AppEndpointName } from "./framework/definitions";
 
 const mockedAgifyResponses = [1, 2, 3, 4, 5];
-describe("User needs to know what is the *age* for specific *name*", () => {
+describe("feature/name-age", () => {
   it(
-    "happy path",
+    "User needs to know what is the *age* for specific *name* / happy path",
     testFramework.createScenario({
       arrange: testFramework.composeSection(
         arrangeClearAgeNamePairTable(),
@@ -35,27 +40,77 @@ describe("User needs to know what is the *age* for specific *name*", () => {
       }),
     }),
   );
+
   it(
-    //? for some reason when 2 `it` are in single file, there is a problem with missing defaultStates in storyteller plugin :(
-    "path number 2",
+    "User needs to know how many times asked for *age* for specific *name* / happy path",
     testFramework.createScenario({
-      arrange: arrangeAgifyEndpoint({ age: mockedAgifyResponses }),
-      act: actNameToAgeBatchEndpoint({ names: ["Tom", "Bob", "Anna", "Hannah", "Alice"] }),
+      arrange: testFramework.composeSection(
+        arrangeClearAgeNamePairTable(),
+        arrangeAgifyEndpoint({ age: mockedAgifyResponses }),
+      ),
+      act: testFramework.composeSection(
+        actNameToAgeBatchEndpoint({ names: ["Tom", "Tom", "Tom", "Tom", "Tom"] }),
+        actRequestsForAgeEndpoint({ name: "Tom" }),
+      ),
       assert: testFramework.createStep({
         name: StepName.assert,
         handler: async (valueObject) => {
-          const agifyRequests = valueObject.expressGetExecutions({ endpointName: ApiEndpointName.getAge });
           const nameToAgeResponses = valueObject.axiosGetResponses({ endpointName: AppEndpointName.nameToAge });
-          //? check is what we sent to the app fitting to what app sent to the mock server
-          expect(agifyRequests.map((agifyRequest) => agifyRequest.request.query.name)).toStrictEqual(
-            nameToAgeResponses.map((nameToAgeResponse) => {
-              const path = nameToAgeResponse.response.request.path;
-              return path.substring(path.lastIndexOf("/") + 1);
-            }),
+          const requestsForAgeResponses = valueObject.axiosGetResponses({
+            endpointName: AppEndpointName.requestsForAge,
+          });
+          expect(nameToAgeResponses.length).toStrictEqual(requestsForAgeResponses[0].response.data.amount);
+        },
+      }),
+    }),
+  );
+
+  it(
+    "User needs to know what is the *average age* for all people / happy path",
+    testFramework.createScenario({
+      arrange: testFramework.composeSection(
+        arrangeClearAgeNamePairTable(),
+        arrangeAgifyEndpoint({ age: mockedAgifyResponses }),
+      ),
+      act: testFramework.composeSection(
+        actNameToAgeBatchEndpoint({ names: ["Tom", "Bob", "Anna", "Hannah", "Alice"] }),
+        actAverageAgeEndpoint(),
+      ),
+      assert: testFramework.createStep({
+        name: StepName.assert,
+        handler: async (valueObject) => {
+          const nameToAgeResponses = valueObject.axiosGetResponses({ endpointName: AppEndpointName.averageAge });
+          expect(nameToAgeResponses.length).toStrictEqual(1);
+          expect(nameToAgeResponses[0].response.data.age).toStrictEqual(
+            nameToAgeResponses
+              .map((nameToAgeResponse) => nameToAgeResponse.response.data.age)
+              .reduce((acc, val) => acc + val, 0) / nameToAgeResponses.length,
           );
-          //? check is what mock server returned fitting to what app returned
-          expect(mockedAgifyResponses).toStrictEqual(
-            nameToAgeResponses.map((nameToAgeResponse) => nameToAgeResponse.response.data.age),
+        },
+      }),
+    }),
+  );
+
+  it(
+    "User needs to know what is the *average age* for specific name / happy path",
+    testFramework.createScenario({
+      arrange: testFramework.composeSection(
+        arrangeClearAgeNamePairTable(),
+        arrangeAgifyEndpoint({ age: mockedAgifyResponses }),
+      ),
+      act: testFramework.composeSection(
+        actNameToAgeBatchEndpoint({ names: ["Tom", "Tom", "Tom", "Tom", "Tom"] }),
+        actAverageAgeForNameEndpoint({ name: "Tom" }),
+      ),
+      assert: testFramework.createStep({
+        name: StepName.assert,
+        handler: async (valueObject) => {
+          const nameToAgeResponses = valueObject.axiosGetResponses({ endpointName: AppEndpointName.averageAgeForName });
+          expect(nameToAgeResponses.length).toStrictEqual(1);
+          expect(nameToAgeResponses[0].response.data.age).toStrictEqual(
+            nameToAgeResponses
+              .map((nameToAgeResponse) => nameToAgeResponse.response.data.age)
+              .reduce((acc, val) => acc + val, 0) / nameToAgeResponses.length,
           );
         },
       }),
